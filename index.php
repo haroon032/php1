5d161e9855906053e46d473e273b779550fd2771
umer <da7916735@gmail.com>
2024-03-07 20:35:00 +0500

    fit

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+    $name = "Hello Bhai"; //string
+    $age = 40;          //INT
+    $isCnic = true;     //BOOLEAN
+    $marks = 90.66;     //FLOAT
+
+
+   echo  "$name";
+   echo  "<br>$age";
+   echo  "<br>$isCnic";
+   echo  "<br>$marks";
+
+   // ARITHMETIC OPERATIONS
+
+  $a = 20;
+  $b = 30;
+  $c = $a + $b;
+  $d = $a - $b;
+  $e = $a * $b;
+  $f = $a / $b;
+  $g = $a % $b;
+  
+
+  echo "<br> he sum of $a + $b is : " .$c;
+  echo "<br> he sub of $a - $b is : " .$d;
+  echo "<br> he mul of $a * $b is : " .$e;
+  echo "<br> he div of $a / $b is : " .$f;
+  echo "<br> he mod of $a % $b is : " .$c;
+ 
+  // PRE INCREMENT
+   $apple = 70;
+   // post INCREMENT
+   echo "<br> " .$apple++ ; //
+   echo "<br> " .$apple;
+
+   //pre increment
+  echo "<br> " .++$apple; //,
+
+  echo "<br> " .--$apple; 
+
+   $co = 200;
+   echo "<br>" .$co-5; 
+    echo "<br>" .$co;
+   
+    $mango = 200;
+    echo "<br> $mango";
+    $mango -=5;
+    echo "<br> $mango";
+    $mango +=35;
+    echo "<br> $mango";
+
+    echo "<br> $mango";
+    $mango -=65;
+    echo "<br> $mango";
+
+    //loop
+    //for loop
+    
+    for($1)
+
+?>
+
